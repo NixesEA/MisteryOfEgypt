57e70a05069cac4fc361ce999a82b1fdf9c4f6ed
NixesEA <ermakov.anton201133@gmail.com>
2019-02-04 18:45:11 +0300

    inflate pause and res in game

--- build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f070023;
-        public static final int end = 0x7f070041;
-        public static final int gone = 0x7f070049;
-        public static final int invisible = 0x7f070053;
-        public static final int left = 0x7f07005d;
-        public static final int packed = 0x7f07006f;
-        public static final int parent = 0x7f070070;
-        public static final int percent = 0x7f070076;
-        public static final int right = 0x7f07007e;
-        public static final int spread = 0x7f070099;
-        public static final int spread_inside = 0x7f07009a;
-        public static final int start = 0x7f07009f;
-        public static final int top = 0x7f0700af;
-        public static final int wrap = 0x7f0700b7;
+        public static final int end = 0x7f070044;
+        public static final int gone = 0x7f07004d;
+        public static final int invisible = 0x7f070057;
+        public static final int left = 0x7f070061;
+        public static final int packed = 0x7f070074;
+        public static final int parent = 0x7f070075;
+        public static final int percent = 0x7f07007c;
+        public static final int right = 0x7f070084;
+        public static final int spread = 0x7f07009f;
+        public static final int spread_inside = 0x7f0700a0;
+        public static final int start = 0x7f0700a5;
+        public static final int top = 0x7f0700b5;
+        public static final int wrap = 0x7f0700bd;
     }
     public static final class styleable {
         private styleable() {}
--- src/main/java/ru/pushapp/misteryofegypt/GameActivity.java
+++ src/main/java/ru/pushapp/misteryofegypt/GameActivity.java
@@ -8,10 +8,24 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.unity3d.player.UnityPlayer;
 
-public class GameActivity extends Activity {
+public class GameActivity extends Activity implements View.OnClickListener {
+
+    boolean showPause = false;
+
+    ImageButton contiue;
+    ImageButton backToMenu;
+    ImageButton pauseBackToMenu;
+
+
+    ImageButton pause;
+    TextView currentScore;
+    TextView currentCountOfLife;
+    FrameLayout pauseLayout;
 
     protected UnityPlayer mUnityPlayer; // don't change the name of this variable; referenced from native code
 
@@ -19,6 +33,24 @@ public class GameActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+//        pauseBackToMenu = findViewById(R.id.pause_exit_menu);
+//        pauseBackToMenu.setOnClickListener(this);
+
+//        backToMenu = findViewById(R.id.btn_exit_menu);
+//        backToMenu.setOnClickListener(this);
+
+//        contiue = findViewById(R.id.pause_continue);
+//        contiue.setOnClickListener(this);
+
+        pause = findViewById(R.id.btn_pause);
+        pause.setOnClickListener(this);
+
+        currentScore = findViewById(R.id.coins_text);
+        currentCountOfLife = findViewById(R.id.life_text);
+
+        pauseLayout = findViewById(R.id.pause_frame);
+
         mUnityPlayer = new UnityPlayer(this);
         mUnityPlayer.requestFocus();
 
@@ -28,33 +60,68 @@ public class GameActivity extends Activity {
 
         View playerView = mUnityPlayer.getView();
         FrameLayout layout = findViewById(R.id.unity_frame);
-        layout.addView(playerView,0);
+        layout.addView(playerView, 0);
     }
 
 
     public void setTextView(String msg) {
 //        Log.i("TEST", "setTextView: " + msg);
-//        testMessage = "Message = " + msg;
-//        textView.setText(testMessage);
     }
 
     public void updateLife(String msg) {
-        int life = Integer.valueOf(msg);
-        Log.i("test update", "file: " + msg);
-        Log.i("test update", "file: " + life);
+//        int life = Integer.valueOf(msg);
+//        Log.i("test update", "file: " + msg);
+//        Log.i("test update", "file: " + life);
 
-//        testMessage = "Message = " + msg;
-//        textView.setText(testMessage);
+        currentCountOfLife.setText(msg);
+        if (msg.equals("0")){
+            mUnityPlayer.pause();
+
+            View infl = getLayoutInflater().inflate(R.layout.fragment_result, null);
+            pauseLayout.addView(infl);
+            showPause = true;
+        }
     }
+
     public void updateMoney(String msg) {
         Log.i("test update", "money: " + msg);
-//        testMessage = "Message = " + msg;
-//        textView.setText(testMessage);
+        currentScore.setText(msg);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_pause:
+                if (!showPause) {
+                    mUnityPlayer.pause();
 
-    @Override protected void onNewIntent(Intent intent)
-    {
+                    View infl = getLayoutInflater().inflate(R.layout.fragment_pause, null);
+                    pauseLayout.addView(infl);
+                    showPause = true;
+                } else {
+                    mUnityPlayer.resume();
+                    pauseLayout.removeAllViews();
+                    showPause = false;
+                }
+                break;
+            case R.id.pause_continue:
+                mUnityPlayer.resume();
+                pauseLayout.removeAllViews();
+                showPause = false;
+                break;
+            case R.id.btn_exit_menu:
+                finish();
+                break;
+            case R.id.pause_exit_menu:
+                finish();
+                break;
+
+        }
+
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
         // To support deep linking, we need to make sure that the client can get access to
         // the last sent intent. The clients access this through a JNI api that allows them
         // to get the intent set on launch. To update that after launch we have to manually
@@ -63,70 +130,69 @@ public class GameActivity extends Activity {
     }
 
     // Quit Unity
-    @Override protected void onDestroy ()
-    {
-        Log.i("life","onDestr");
+    @Override
+    protected void onDestroy() {
+        Log.i("life", "onDestr");
         mUnityPlayer.destroy();
         super.onDestroy();
     }
 
     // Pause Unity
-    @Override protected void onPause()
-    {
+    @Override
+    protected void onPause() {
 //        mUnityPlayer.pause();
-        Log.i("life","onPause");
+        Log.i("life", "onPause");
         super.onPause();
     }
 
     // Resume Unity
-    @Override protected void onResume()
-    {
-        Log.i("life","onResume");
+    @Override
+    protected void onResume() {
+        Log.i("life", "onResume");
         super.onResume();
         mUnityPlayer.resume();
     }
 
-    @Override protected void onStart()
-    {
-        Log.i("life","onStart");
+    @Override
+    protected void onStart() {
+        Log.i("life", "onStart");
         super.onStart();
         mUnityPlayer.start();
     }
 
-    @Override protected void onStop()
-    {
-        Log.i("life","onStop");
+    @Override
+    protected void onStop() {
+        Log.i("life", "onStop");
         super.onStop();
         mUnityPlayer.stop();
     }
 
     // Low Memory Unity
-    @Override public void onLowMemory()
-    {
+    @Override
+    public void onLowMemory() {
         super.onLowMemory();
         mUnityPlayer.lowMemory();
     }
 
     // Trim Memory Unity
-    @Override public void onTrimMemory(int level)
-    {
+    @Override
+    public void onTrimMemory(int level) {
         super.onTrimMemory(level);
-        if (level == TRIM_MEMORY_RUNNING_CRITICAL)
-        {
+        if (level == TRIM_MEMORY_RUNNING_CRITICAL) {
             mUnityPlayer.lowMemory();
         }
     }
 
     // This ensures the layout will be correct.
-    @Override public void onConfigurationChanged(Configuration newConfig)
-    {
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         mUnityPlayer.configurationChanged(newConfig);
     }
 
     // Notify Unity of the focus change.
-    @Override public void onWindowFocusChanged(boolean hasFocus)
-    {
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         mUnityPlayer.windowFocusChanged(hasFocus);
     }
